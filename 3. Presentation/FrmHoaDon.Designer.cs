09286608cdd74bc78c90670c906e3dd1ab9dfd4b
Admin <Admin@DESKTOP-FOSRL05>
2022-08-05 10:29:16 +0700

    code FrmHoaDon

--- 3. Presentation/FrmHoaDon.Designer.cs
+++ 3. Presentation/FrmHoaDon.Designer.cs
@@ -32,9 +32,20 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.dtg_hoadonchitiet = new System.Windows.Forms.DataGridView();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btn_timk = new System.Windows.Forms.Button();
             this.dtg_hoadon = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tbt_timk = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
@@ -48,17 +59,20 @@
             // 
             this.panel1.Controls.Add(this.groupBox2);
             this.panel1.Controls.Add(this.groupBox1);
-            this.panel1.Location = new System.Drawing.Point(-5, -4);
+            this.panel1.Location = new System.Drawing.Point(-6, -5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(810, 458);
+            this.panel1.Size = new System.Drawing.Size(926, 611);
             this.panel1.TabIndex = 0;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.dtg_hoadonchitiet);
-            this.groupBox2.Location = new System.Drawing.Point(17, 237);
+            this.groupBox2.Location = new System.Drawing.Point(19, 316);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(776, 205);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(887, 273);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Hóa đơn chi tiết";
@@ -66,11 +80,46 @@
             // dtg_hoadonchitiet
             // 
             this.dtg_hoadonchitiet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtg_hoadonchitiet.Location = new System.Drawing.Point(11, 22);
+            this.dtg_hoadonchitiet.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11});
+            this.dtg_hoadonchitiet.Location = new System.Drawing.Point(13, 29);
+            this.dtg_hoadonchitiet.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dtg_hoadonchitiet.Name = "dtg_hoadonchitiet";
+            this.dtg_hoadonchitiet.RowHeadersWidth = 51;
             this.dtg_hoadonchitiet.RowTemplate.Height = 25;
-            this.dtg_hoadonchitiet.Size = new System.Drawing.Size(759, 177);
+            this.dtg_hoadonchitiet.Size = new System.Drawing.Size(867, 236);
             this.dtg_hoadonchitiet.TabIndex = 4;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Mã Sản Phẩm";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 125;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Tên Sản Phẩm";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 125;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Số Lượng";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 125;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Đơn Giá";
+            this.Column11.MinimumWidth = 6;
+            this.Column11.Name = "Column11";
+            this.Column11.Width = 125;
             // 
             // groupBox1
             // 
@@ -78,9 +127,11 @@
             this.groupBox1.Controls.Add(this.dtg_hoadon);
             this.groupBox1.Controls.Add(this.tbt_timk);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(17, 16);
+            this.groupBox1.Location = new System.Drawing.Point(19, 21);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(776, 215);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(887, 287);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Hóa đơn";
@@ -90,47 +141,111 @@
             this.btn_timk.BackColor = System.Drawing.Color.Aquamarine;
             this.btn_timk.Image = ((System.Drawing.Image)(resources.GetObject("btn_timk.Image")));
             this.btn_timk.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_timk.Location = new System.Drawing.Point(627, 16);
+            this.btn_timk.Location = new System.Drawing.Point(731, 21);
+            this.btn_timk.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_timk.Name = "btn_timk";
-            this.btn_timk.Size = new System.Drawing.Size(85, 23);
+            this.btn_timk.Size = new System.Drawing.Size(97, 31);
             this.btn_timk.TabIndex = 3;
             this.btn_timk.Text = "Tìm kiếm";
             this.btn_timk.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_timk.UseVisualStyleBackColor = false;
+            this.btn_timk.Click += new System.EventHandler(this.btn_timk_Click);
             // 
             // dtg_hoadon
             // 
             this.dtg_hoadon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtg_hoadon.Location = new System.Drawing.Point(11, 44);
+            this.dtg_hoadon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dtg_hoadon.Location = new System.Drawing.Point(13, 59);
+            this.dtg_hoadon.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dtg_hoadon.Name = "dtg_hoadon";
+            this.dtg_hoadon.RowHeadersWidth = 51;
             this.dtg_hoadon.RowTemplate.Height = 25;
-            this.dtg_hoadon.Size = new System.Drawing.Size(759, 165);
+            this.dtg_hoadon.Size = new System.Drawing.Size(867, 220);
             this.dtg_hoadon.TabIndex = 2;
+            this.dtg_hoadon.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtg_hoadon_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mã Hóa Đơn";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Ngày Tạo";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Email Nhân Viên";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Số Điện Thoại Khách Hàng";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 135;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Tổng Giá";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Trạng Thái";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Ghi Chú";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 125;
             // 
             // tbt_timk
             // 
-            this.tbt_timk.Location = new System.Drawing.Point(171, 16);
+            this.tbt_timk.Location = new System.Drawing.Point(205, 22);
+            this.tbt_timk.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tbt_timk.Name = "tbt_timk";
-            this.tbt_timk.Size = new System.Drawing.Size(450, 23);
+            this.tbt_timk.Size = new System.Drawing.Size(514, 27);
             this.tbt_timk.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(57, 19);
+            this.label1.Location = new System.Drawing.Point(65, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 15);
+            this.label1.Size = new System.Drawing.Size(135, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tìm kiếm hóa đơn";
             // 
             // FrmHoaDon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Goldenrod;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FrmHoaDon";
             this.Text = "FrmHoaDon";
             this.panel1.ResumeLayout(false);
@@ -153,5 +268,16 @@
         private DataGridView dtg_hoadonchitiet;
         private Button btn_timk;
         private DataGridView dtg_hoadon;
+        private DataGridViewTextBoxColumn Column8;
+        private DataGridViewTextBoxColumn Column9;
+        private DataGridViewTextBoxColumn Column10;
+        private DataGridViewTextBoxColumn Column11;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
     }
 }
