5ede09ec053738cff8c9d61c1c1b9f36db4c3d8a
Admin <Admin@DESKTOP-FOSRL05>
2022-08-10 09:56:52 +0700

    dang xuat

--- 3. Presentation/FrmMain.cs
+++ 3. Presentation/FrmMain.cs
@@ -6,19 +6,21 @@ namespace _3._Presentation
     public partial class FrmMain : Form
     {
         IQLEmployeeServices _iQLEmployee;
+        IQLRoleServices _iQLRole;
 
-        
-        
+
+
         public FrmMain()
         {
             InitializeComponent();
             _iQLEmployee = new QLEmployeeServices();
+            _iQLRole = new QLRoleServices();
             this.CenterToScreen();
             GoFullscreen(AutoSize);
             panel_ttnv.Visible = false;
 
         }
-        
+
         private void FrmMain_Load(object sender, EventArgs e)
         {
             var layEmail = Properties.Settings.Default.TKdaLogin;
@@ -26,7 +28,8 @@ namespace _3._Presentation
             //string linkanh = nhanvien.LinkAnh.Replace(@"\", @"/");
             //pic_avtNV.Image = Image.FromFile(linkanh);
             //pic_avtNV.SizeMode = PictureBoxSizeMode.StretchImage;
-            lb_tenNV.Text = "NV : " + nhanvien.FullName;
+            var role = _iQLRole.GetRoleFromDB().FirstOrDefault(x => x.ID == nhanvien.IDRoles);
+            lb_tenNV.Text = role.RoleName + ": " + nhanvien.FullName;
         }
         private Form activeForm;
         public void ChangeForm(Form form)
@@ -63,13 +66,13 @@ namespace _3._Presentation
         private void btn_nv_Click(object sender, EventArgs e)
         {
 
-            var idRole = _iQLEmployee.GetEmployeeFromDB().Where(p => p.Email == Properties.Settings.Default.TKdaLogin).Select(p=>p.IDRoles).FirstOrDefault();
+            var idRole = _iQLEmployee.GetEmployeeFromDB().Where(p => p.Email == Properties.Settings.Default.TKdaLogin).Select(p => p.IDRoles).FirstOrDefault();
             if (idRole == 1)
             {
                 FrmNhanVien frmNhanVien = new FrmNhanVien();
                 ChangeForm(frmNhanVien);
             }
-            else if (idRole != 1 )
+            else if (idRole != 1)
             {
                 MessageBox.Show("Nhân viên không có quyền sử dụng chức năng này");
             }
@@ -192,7 +195,14 @@ namespace _3._Presentation
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-
+            DialogResult dialogResult = MessageBox.Show("Bạn có chắc muốn đăng xuất không?", "Đăng xuất", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.Hide();
+                FrmLogin frmLogin = new FrmLogin();
+                frmLogin.ShowDialog();
+                //this.Close();
+            }
         }
 
         private void btn_KhachHang_Click(object sender, EventArgs e)
